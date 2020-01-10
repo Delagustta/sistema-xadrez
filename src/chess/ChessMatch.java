0e6000d01f08322a3ebdb62ebad13ffa92fc2961
Gustavo Funchal <delagusttaf@gmail.com>
2020-01-10 10:38:27 -0300

    handling exceptions e limpando a tela

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -49,7 +49,7 @@ public class ChessMatch {
 	
 	private void validateSourcePosition(Position position) {
 		if (!board.thereIsAPiece(position)) {
-			throw new ChessException("Não existe peça na posição de origem.");
+			throw new ChessException("There is no piece on source position");
 		}
 	}
 	
